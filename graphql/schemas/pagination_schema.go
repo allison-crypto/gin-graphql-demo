ca8d30cd112f4cf91f19a2922307c4b764bb0738
Allison Wu <allison.wu@crypto.com>
2021-11-08 12:45:59 +0800

    rename schema file

--- graphql/schemas/pagination_schema.go
+++ graphql/schemas/pagination_schema.go
@@ -12,18 +12,20 @@ type testData struct {
 var paginationFiled = graphqlField(
 	pagenationType("pagination", developerType),
 	"pagination recrods",
-	func(p graphql.ResolveParams) (interface{}, error) {
-		return PaginationResp{
-			Data: []testData{
-				{Name: "allison", Task: "api"},
-				{Name: "samson", Task: "lambda"},
-			},
-			Page:     1,
-			PageSize: 2,
-			Total:    2,
-		}, nil
-	},
+	paginationReslove,
 )
+
+func paginationReslove(p graphql.ResolveParams) (interface{}, error) {
+	return PaginationResp{
+		Data: []testData{
+			{Name: "allison", Task: "api"},
+			{Name: "samson", Task: "lambda"},
+		},
+		Page:     1,
+		PageSize: 2,
+		Total:    2,
+	}, nil
+}
 
 var developerType = graphql.NewObject(graphql.ObjectConfig{
 	Name:        "developer",
